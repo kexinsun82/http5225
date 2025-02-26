67954623ee957349ddda2c4326c4bc5b04eefe07
Kexin Sun <kexin.sun82@gmail.com>
2025-02-25 23:33:59 -0500

    Add Assignment1

--- assignment1/index.php
+++ assignment1/index.php
@@ -13,36 +13,41 @@
 <body>
   <!-- nav.php -->
   <?php include('common/nav.php') ?>
-  <div class="container-fluid">
+  <div class="container mt-4">
     <div class="container">
-      <div class="row">
-        <div class="col">
-          <h1 class="display-5">All Foods</h1>
-        </div>
-      </div>
+      <h1 class="display-5 text-center mb-4">Food Nutrition Data</h1>
       <div class="row">
         <!-- connection.php -->
         <?php
         include('common/connection.php');
         
-        $query = "SELECT f.id, f.food_item, c.category_name, f.calories 
-                FROM food f 
-                JOIN category c ON f.category_id = c.id";
+        $query = "SELECT f.id, f.food_item, c.category_name, 
+                       f.calories, f.protein, f.fat, 
+                       f.carbohydrates, f.sugars 
+                  FROM food f
+                  JOIN category c ON f.category_id = c.id";
         $foods = mysqli_query($connect, $query);
 
         foreach ($foods as $food) {
+          $encrypted_food = base64_encode($food['food_item']);
+
           echo '<div class="col-md-4">
-                  <div class="card mb-4" style="">
-                    <div class="card-body">
-                      <h5 class="card-title">' . $food['food_item'] . '</h5>
-                      <span class="badge rounded-pill bg-primary">' . $food['category_name'] . '</span>
-                      <span class="badge rounded-pill bg-success">' . $food["calories"] . '</span>
-                    </div>
+                <div class="card mb-4 shadow">
+                  <div class="card-body">
+                    <h5 class="card-title text-primary">' . $food['food_item'] . '</h5>
+                    <span class="badge bg-info">' . $food['category_name'] . '</span>
+                    <p class="mt-2">
+                      <strong>Calories:</strong> ' . $food['calories'] . ' kcal<br>
+                      <strong>Protein:</strong> ' . $food['protein'] . ' g<br>
+                      <strong>Fat:</strong> ' . $food['fat'] . ' g<br>
+                      <strong>Carbs:</strong> ' . $food['carbohydrates'] . ' g<br>
+                      <strong>Sugars:</strong> ' . $food['sugars'] . ' g
+                    </p>
                   </div>
-                </div>';
+                </div>
+              </div>';
         }
     ?>
-      </div>
     </div>
   </div>
 </body>
